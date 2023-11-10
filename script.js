9f0f1ec10c18d551041343bef0f8e54b90a81376
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-10 18:35:42 +0530

    added JSdoc

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ submit.addEventListener('click', () => {
 })
 
 // Functions
+
+/**
+ * Highlights a button by changing its background color.
+ *
+ * @param {HTMLElement} element - The button element to be highlighted.
+ */
 const highlightButton = (element) => {
 	resetButtons()
 
@@ -23,12 +29,23 @@ const highlightButton = (element) => {
 	highlighted = element.parentElement.children[1].innerText
 }
 
+/**
+ * Resets the background color of all buttons in the feedback array to '#1D3557'.
+ *
+ * @param {Array} feedback - the array of buttons to be reset
+ * @return {undefined} - no return value
+ */
 const resetButtons = () => {
 	Array.from(feedback).forEach((button) => {
 		button.style.backgroundColor = '#1D3557'
 	})
 }
 
+/**
+ * Handles modal based on the value of `highlighted`.
+ *
+ * @return {void} No return value.
+ */
 const handleModal = () => {
 	if (highlighted === 'Sad') {
 		sad.showModal()
